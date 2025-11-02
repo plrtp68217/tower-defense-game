a1c31550b19f97c1dc31e6546e1cfe5a18579ccc
plrtp <plrtp@mail.ru>
2025-11-02 17:17:00 +0300

    update: проверка на коллизии перед размезщением, отображение превью размещаемого объекта, отображение состояния размещения: белое - можно, красное - нельзя

---
+++ Assets/Scripts/GridData.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GridData
+{
+    private readonly Dictionary<Vector3Int, PlacementData> _placedObjects = new();
+
+    public void AddObject(Vector3Int gridPosition, Vector2Int objectSize, int ID, int objectIndex)
+    {
+        List<Vector3Int> positionsToOccupy = CalculatePositions(gridPosition, objectSize);
+
+        PlacementData data = new (positionsToOccupy, ID, objectIndex);
+
+        bool placementValidity = CanPlaceObject(gridPosition, objectSize);
+
+        if (placementValidity == false)
+        {
+            return;
+        }
+
+        foreach (var position in positionsToOccupy)
+        {
+            _placedObjects[position] = data;
+        }
+    }
+
+    public bool CanPlaceObject(Vector3Int gridPosition, Vector2Int objectSize)
+    {
+        List<Vector3Int> postionsToOccupy = CalculatePositions(gridPosition, objectSize);
+
+        foreach (var position in postionsToOccupy)
+        {
+            if (_placedObjects.ContainsKey(position))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<Vector3Int> CalculatePositions(Vector3Int gridPosition, Vector2Int objectSize)
+    {
+        List<Vector3Int> returnValue = new();
+
+        for (int x = 0;  x < objectSize.x; x++)
+        {
+            for (int y = 0; y < objectSize.y; y++)
+            {
+                returnValue.Add(gridPosition + new Vector3Int(x, 0, y));
+            }
+        }
+
+        return returnValue;
+    }
+}
+
+public class PlacementData
+{
+    public List<Vector3Int> occupiedPositions;
+
+    public int  ID { get; private set; }
+
+    public int  PlacedObjectIndex { get; private set; }
+
+    public PlacementData(List<Vector3Int> occupiedPositions, int ID, int placedObjectIndex)
+    {
+        this.occupiedPositions = occupiedPositions;
+        this.ID = ID;
+        PlacedObjectIndex = placedObjectIndex;
+    }
+}
--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -1,10 +1,12 @@
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _mouseIndicator, _cellIndicator;
+    private GameObject _mouseIndicator;
 
     [SerializeField]
     private InputManager _inputManager;
@@ -18,7 +20,17 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private GameObject _gridVisualisation;
 
+    [SerializeField]
+    private AudioSource _audioSourceSuccess;
+
     private int _selectedObjectIndex = -1;
+
+    private GridData _gridData;
+
+    private List<GameObject> _placedGameObjects = new();
+
+    [SerializeField]
+    private PreviewSystem _previewSystem;
 
     public void StartPlacement(int ID)
     {
@@ -33,7 +45,11 @@ public class PlacementSystem : MonoBehaviour
         }
 
         _gridVisualisation.SetActive(true);
-        _cellIndicator.SetActive(true);
+
+        _previewSystem.StartShowingPlacementPreview(
+            _database.objectsData[_selectedObjectIndex].Prefab,
+            _database.objectsData[_selectedObjectIndex].Size
+            );
 
         _inputManager.OnClicked += PlaceStructure;
         _inputManager.OnExit += StopPlacement;
@@ -44,7 +60,8 @@ public class PlacementSystem : MonoBehaviour
         _selectedObjectIndex = -1;
 
         _gridVisualisation.SetActive(false);
-        _cellIndicator.SetActive(false);
+
+        _previewSystem.StopShowingPlacementPreview();
 
         _inputManager.OnClicked -= PlaceStructure;
         _inputManager.OnExit -= StopPlacement;
@@ -60,15 +77,44 @@ public class PlacementSystem : MonoBehaviour
         Vector3 mousePosition = _inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = _grid.WorldToCell(mousePosition);
 
+        bool placementValidity = CheckPlacementValidity(gridPosition, _selectedObjectIndex);
+
+        if (placementValidity == false)
+        {
+            return;
+        }
+
         GameObject newObject = Instantiate(_database.objectsData[_selectedObjectIndex].Prefab);
         newObject.transform.position = _grid.CellToWorld(gridPosition);
 
+        _placedGameObjects.Add(newObject);
+
+        _gridData.AddObject(
+            gridPosition, 
+            _database.objectsData[_selectedObjectIndex].Size,
+            _database.objectsData[_selectedObjectIndex].ID,
+            _placedGameObjects.Count - 1
+            );
+
+        _audioSourceSuccess.Play();
+
         StopPlacement();
+    }
+
+    private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
+    {
+        Vector2Int objectSize = _database.objectsData[selectedObjectIndex].Size;
+        bool placementValidity = _gridData.CanPlaceObject(gridPosition, objectSize);
+        return placementValidity;
     }
 
     private void Start()
     {
         StopPlacement();
+
+        _mouseIndicator.SetActive(false);
+
+        _gridData = new();
     }
 
     private void Update()
@@ -81,7 +127,10 @@ public class PlacementSystem : MonoBehaviour
         Vector3 mousePosition = _inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = _grid.WorldToCell(mousePosition);
 
+        bool placementValidity = CheckPlacementValidity(gridPosition, _selectedObjectIndex);
+
+        _previewSystem.UpdatePosition(_grid.CellToWorld(gridPosition), placementValidity);
+
         _mouseIndicator.transform.position = mousePosition;
-        _cellIndicator.transform.position = _grid.CellToWorld(gridPosition);
     }
 }
